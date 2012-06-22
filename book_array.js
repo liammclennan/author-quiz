dc221d9a037e518ad48ff5f4c39e07f763ffd73a
Liam McLennan <liam.mclennan@gmail.com>
2012-06-23 08:06:23 +1000

    added data and script to load books into server

---
+++ book_array.js
@@ -0,0 +1,24 @@
+{"title": "Pride Prejudice", "authorImg": "austen.jpg"}
+{"title": "Heart of Darkness", "authorImg": "conrad.jpg"}
+{"title": "Lord Jim", "authorImg": "conrad.jpg"}
+{"title": "Tender is the Night", "authorImg": "fitzgerald.jpg"}
+{"title": "The Sun Also Rises", "authorImg": "hemingway.jpg"}
+{"title": "The Old Man and The Sea", "authorImg": "hemingway.jpg"}
+{"title": "The Stand", "authorImg": "king.jpg"}
+{"title": "Misery", "authorImg": "king.jpg"}
+{"title": "Moby Dick", "authorImg": "melville.jpg"}
+{"title": "1984", "authorImg": "orwell.jpg"}
+{"title": "Animal Farm", "authorImg": "orwell.jpg"}
+{"title": "The Cask of Amontillado", "authorImg": "poe.jpg"}
+{"title": "The Raven", "authorImg": "poe.jpg"}
+{"title": "Midnights Children", "authorImg": "rushdie.jpg"}
+{"title": "Satanic Verses", "authorImg": "rushdie.jpg"}
+{"title": "Frankenstein", "authorImg": "shelley.jpg"}
+{"title": "Dracula", "authorImg": "stoker.jpg"}
+{"title": "Walden", "authorImg": "thoreau.jpg"}
+{"title": "Civil Disobedience", "authorImg": "thoreau.jpg"}
+{"title": "Lord of the Rings", "authorImg": "tolkein.jpg"}
+{"title": "The Hobbit", "authorImg": "tolkein.jpg"}
+{"title": "War and Peace", "authorImg": "tolstoy.jpg"}
+{"title": "Adventures of Huckleberry Finn", "authorImg": "twain.jpg"}
+{"title": "The Awful German Language", "authorImg": "twain.jpg"}
